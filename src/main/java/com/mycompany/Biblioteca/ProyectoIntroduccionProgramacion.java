a182451d25dea941f11def62ec1cec9591549924
JulianQuesada27 <jquesada80228@ufide.ac.cr>
2026-07-23 11:07:43 -0600

    creacion de todas las clases

---
+++ src/main/java/com/mycompany/Biblioteca/ProyectoIntroduccionProgramacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectointroduccionprogramacion;
+
+/**
+ *
+ * @author julianquesadamora
+ */
+public class ProyectoIntroduccionProgramacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
