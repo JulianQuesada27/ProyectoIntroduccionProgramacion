9734cf1ae426cb5965785025f45ca082589b09c0
Matias122522 <mreyes60824@ufide.ac.cr>
2026-08-09 13:51:41 -0600

    Agrego Prestamos y Devoluciones

--- src/main/java/com/mycompany/Biblioteca/Devolucion.java
+++ src/main/java/com/mycompany/Biblioteca/Devolucion.java
@@ -8,6 +8,45 @@ package com.mycompany.Biblioteca;
  *
  * @author julianquesadamora
  */
+
 public class Devolucion {
-    
+    private String codigoLibro;
+    private String idUsuario;
+    private String fechaDevolucion;
+
+    public Devolucion(String codigoLibro, String idUsuario, String fechaDevolucion) {
+        this.codigoLibro = codigoLibro;
+        this.idUsuario = idUsuario;
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public String getCodigoLibro() {
+        return codigoLibro;
+    }
+
+    public void setCodigoLibro(String codigoLibro) {
+        this.codigoLibro = codigoLibro;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public String mostrarInfo() {
+        return "Código Libro: " + codigoLibro + 
+               " | ID Usuario: " + idUsuario + 
+               " | Fecha Devolución: " + fechaDevolucion;
+    }
 }
--- src/main/java/com/mycompany/Biblioteca/Prestamo.java
+++ src/main/java/com/mycompany/Biblioteca/Prestamo.java
@@ -8,6 +8,58 @@ package com.mycompany.Biblioteca;
  *
  * @author julianquesadamora
  */
+
+
+
 public class Prestamo {
-    
+    private String codigoLibro;
+    private String idUsuario;
+    private String fechaPrestamo;
+    private String fechaEntrega;
+
+    public Prestamo(String codigoLibro, String idUsuario, String fechaPrestamo, String fechaEntrega) {
+        this.codigoLibro = codigoLibro;
+        this.idUsuario = idUsuario;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public String getCodigoLibro() {
+        return codigoLibro;
+    }
+
+    public void setCodigoLibro(String codigoLibro) {
+        this.codigoLibro = codigoLibro;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public String getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(String fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public String mostrarInfo() {
+        return "Código Libro: " + codigoLibro + 
+               " | ID Usuario: " + idUsuario + 
+               " | Fecha Préstamo: " + fechaPrestamo + 
+               " | Fecha Entrega: " + fechaEntrega;
+    }
 }
